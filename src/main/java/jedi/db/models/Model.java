7ada21ba6f1995ac94a703db5ae85405894ba19a
Thiago Alexandre Martins Monteiro <thiago.amm.agr@gmail.com>
2017-09-29 15:10:54 -0300

    Removida a implementação local de hashCode.

--- src/main/java/jedi/db/models/Model.java
+++ src/main/java/jedi/db/models/Model.java
@@ -735,14 +735,6 @@ public class Model implements IModel {
    }
    
    @Override
-   public int hashCode() {
-      final int prime = 31;
-      int result = 1;
-      result = prime * result + id;
-      return result;
-   }
-   
-   @Override
    public boolean equals(Object obj) {
       if (this == obj) return true;
       if (obj == null) return false;
